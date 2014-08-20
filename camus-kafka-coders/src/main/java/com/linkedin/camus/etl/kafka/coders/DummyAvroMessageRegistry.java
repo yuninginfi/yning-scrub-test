ed93291300b59e2f10bbff494134779dd9b617ba
Yu Ning <yning@rocketfuel.com>
2014-08-20 14:48:57 -0700

    commit fix

--- camus-kafka-coders/src/main/java/com/linkedin/camus/etl/kafka/coders/DummyAvroMessageRegistry.java
+++ camus-kafka-coders/src/main/java/com/linkedin/camus/etl/kafka/coders/DummyAvroMessageRegistry.java
@@ -7,8 +7,10 @@ import com.linkedin.camus.schemaregistry.MemorySchemaRegistry;
 
 public class DummyAvroMessageRegistry extends MemorySchemaRegistry<Schema>{
     Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"DummyLog\",\"namespace\":\"com.linkedin.camus.etl.kafka.coders\",\"doc\":\"Logs for not so important stuff.\",\"fields\":[{\"name\":\"id\",\"type\":\"long\"},{\"name\":\"logTime\",\"type\":\"long\"}]}");
+    Schema SCHEMA2$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"DummyLog\",\"namespace\":\"com.linkedin.camus.etl.kafka.coders\",\"doc\":\"Logs for not so important stuff.\",\"fields\":[{\"name\":\"eventType\",\"type\":\"string\"},{\"name\":\"timestamp\",\"type\":\"string\"},{\"name\":\"bucketId\",\"type\":\"string\"},{\"name\":\"gogogo\",\"type\":\"string\"}]}"); 
     public DummyAvroMessageRegistry() {
         super();
         super.register("DummyLog", SCHEMA$);
+        super.register("partition_test", SCHEMA2$);
     }
 }
